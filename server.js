78af552f17073049aea57a2c85b9f3750072f920
DinuthInduwara <89961332+DinuthInduwara@users.noreply.github.com>
2022-02-01 05:22:19 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+const MONGODB_URI="mongodb+srv://dinuth:dinuth@cluster0.p4ufu.mongodb.net/emmaWatsonDatabace";
+
+const PORT = 4080;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+
+const emmawatsonRouter = require("./routes/emmawatson.js");
+app.use("/emmawatson", emmawatsonRouter);
+
+
+
+
+
+
+
+
+
+
+
+const db = mongoose.connection;
+db.once("open", () =>{
+    console.log("mongo connected");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("server is up and running" , PORT)
+})
